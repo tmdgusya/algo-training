ea2a2e5fc982c91e74386c37520a5fc8435242c2
roach <dev0jsh@gmail.com>
2025-01-20 22:26:01 +0900

    refactored code to use comprehension

--- codeforce/71A.py
+++ codeforce/71A.py
@@ -1,10 +1,6 @@
 
 
 n = int(input())
-words = list(map(
-    lambda s:s[0]+(len(s) - 2)+s[-1] if(len(s) > 10) else s,
-    [input() for _ in range(n)]
-))
-
+words = [word[0]+str(len(word)-2)+word[-1] if len(word) > 10 else word for word in (input() for _ in range(n))]
 for word in words:
     print(word+'\n')
